91d665be1ac4122eaa31250cdf9f44b5316ccb39
AnotherDev <joseluismanco37@gmail.com>
2024-07-07 05:44:35 +0300

    add POND AI

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,10 +2,10 @@ export async function POST(request: Request) {
     const graphResponse = await fetch('https://gateway-arbitrum.network.thegraph.com/api/369a7404bc39d66970805282c50365af/subgraphs/id/8wR23o1zkS4gpLqLNU4kG3JHYVucqGyopL5utGxP2q1N', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          query: `
+            query: `
             {
               reserves {
                 name
@@ -22,10 +22,12 @@ export async function POST(request: Request) {
             }
           `
         })
-      })
-      const graphData = await graphResponse.json()
-      console.debug(graphData)
+    })
+    const graphData = await graphResponse.json()
 
+    const analyticsResponse = await fetch('http://model-v2-api-471546444.us-east-1.elb.amazonaws.com:8001/api/v1/predict/0xae7ab96520de3a18e5e111b5eaab095312d7fe84')
+    const analyticsData = await analyticsResponse.json()
+    console.debug(analyticsData)
     const response = await fetch('https://api.red-pill.ai/v1/chat/completions', {
         method: 'POST',
         headers: {
@@ -37,7 +39,7 @@ export async function POST(request: Request) {
             messages: [
                 {
                     role: 'user',
-                    content: `Give me the best three collaterals to use on aave, give me less tan 100 words,don't mention the subgraph or any relation to this prompt, be so explicit and explain everithing but avoid to include un-redeable values, explain on an easy way why the token is the best option, this is information from the subgraph: ${JSON.stringify(graphData)},
+                    content: `Give me the best three collaterals to use on aave, give me less tan 100 words,don't mention the subgraph or any relation to this prompt, be so explicit and explain everithing but avoid to include un-redeable values, explain on an easy way why the token is the best option, this is information from the subgraph: ${JSON.stringify(graphData)}, and this is information related another AI call POND predicting the future price of many tokens, make it explicit if you use the information to do some recommendations ${JSON.stringify(analyticsData)},
                     at the final of the response give me the colaterals SYMBOL from the subgraph with the next format {{collateral}}`
                 }
             ],
@@ -46,7 +48,7 @@ export async function POST(request: Request) {
     })
     const responseData = await response.json()
 
-    return Response.json({responseData})
+    return Response.json({ responseData })
 
- 
+
 }
